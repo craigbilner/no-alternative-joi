deb16ed8ae89aea29bd7b93808d98db34f5d10e5
Craig Bilner <craig.bilner@gmail.com>
2016-01-16 17:21:51 +0000

    Add test

--- tests.js
+++ tests.js
@@ -1,7 +1,23 @@
 import assert from 'assert';
+import Joi from 'joi';
+
+const mealA = {
+  fortuneCookies: 5,
+  prawnToast: 'yum',
+  chowMein: true,
+};
+
+const mealASchema = {
+  fortuneCookies: Joi.number(),
+  prawnToast: Joi.string(),
+  chowMein: Joi.boolean(),
+};
 
 describe('when I try and make a decision on what chinese meal to eat', () => {
-  it('meal a should be valid', () => {
-    assert(false);
+  it('meal a should be valid', (done) => {
+    Joi.validate(mealA, mealASchema, (err, value) => {
+      assert.deepEqual(value, mealA);
+      done();
+    });
   });
 });

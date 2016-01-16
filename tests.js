3935bd451d47203dfbaf0c4ecb9d6cd527bd2ac3
Craig Bilner <craig.bilner@gmail.com>
2016-01-16 17:01:45 +0000

    Add initial files

---
+++ tests.js
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('when I try and make a decision on what chinese meal to eat', () => {
+  it('meal a should be valid', () => {
+    assert(false);
+  });
+});
